375f30a57fe25ae3b4907cde778a8aff38a58b58
Tiago Danin <TiagoDanin@outlook.com>
2022-05-23 07:45:06 -0300

    feat: restart template files

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,23 +1,3 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    'module:react-native-dotenv',
-    'react-native-reanimated/plugin',
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        extensions: ['.js', '.json'],
-        alias: {
-          '@': './src',
-          '@assets': './src/assets',
-          '@components': './src/components',
-          '@contexts': './src/contexts',
-          '@routes': './src/routes',
-          '@screens': './src/screens',
-          '@utils': './src/utils',
-        },
-      },
-    ],
-  ],
 };
--- template/src/types/env.d.ts
+++
@@ -1,3 +0,0 @@
-declare module '@env' {
-  export const RN_BASE: string;
-}
