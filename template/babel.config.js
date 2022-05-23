ac9fe93aba17faae1412c8c6b6e054274c79e38d
Tiago Danin <TiagoDanin@outlook.com>
2022-05-23 08:29:38 -0300

    feat: configuration base to typescript

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,3 +1,47 @@
+const presets = ['module:metro-react-native-babel-preset'];
+const plugins = [
+  [
+    'module:react-native-dotenv',
+    {
+      envName: 'APP_ENV',
+      moduleName: '@env',
+      path: '.env',
+      blocklist: null,
+      allowlist: null,
+      safe: false,
+      allowUndefined: true,
+      verbose: false,
+    },
+  ],
+];
+
+plugins.push([
+  'module-resolver',
+  {
+    root: ['./src'],
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+    alias: {
+      '@assets': './src/assets',
+      '@components': './src/components',
+      '@contexts': './src/contexts',
+      '@theme': './src/theme',
+      '@typings': './src/typings',
+      '@routes': './src/routes',
+      '@services': './src/services',
+      '@screens': './src/screens',
+      '@utils': './src/utils',
+      '@tests': './src/tests',
+      '@': './src',
+    },
+  },
+]);
+
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets,
+  env: {
+    production: {
+      plugins: ['react-native-paper/babel'],
+    },
+  },
+  plugins,
 };
