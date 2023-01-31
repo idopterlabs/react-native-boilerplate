7e2abcfb478c5b569d5d0f6f15c15abede7ba883
Tiago Danin <TiagoDanin@outlook.com>
2023-01-31 14:20:41 -0300

    feat: create new theme to rn-navigation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,21 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.0.2] - 2022-01-31
+
+### Added
+
+- Adicionado tema do React Navigation
+
+### Changed
+
+- Atualizado patch do React Native Paper para 4.12.4
+
+### Fixed
+
+- Usando cor de fundo da barra de navegação via contexto do tema
+
+
 ## [1.0.2] - 2022-09-26
 
 ### Added
--- src/routes/AuthNavigator.tsx
+++ src/routes/AuthNavigator.tsx
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { ThemeContext } from 'styled-components/native';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Login from '@screens/Auth/Login';
 
-import theme from '@theme/index';
-
 import { AuthStackRouter } from '@typings/routes';
 
 const AuthStack = createNativeStackNavigator<AuthStackRouter>();
 
-export default () => (
-  <AuthStack.Navigator initialRouteName="Login">
-    <AuthStack.Screen
-      name="Login"
-      component={Login}
-      options={() => ({
-        animation: 'slide_from_left',
-        headerShown: true,
-        headerStyle: {
-          backgroundColor: theme.colors.light.primary,
-        },
-      })}
-    />
-  </AuthStack.Navigator>
-);
+export default () => {
+  const themeContext = useContext(ThemeContext);
+
+  return (
+    <AuthStack.Navigator initialRouteName="Login">
+      <AuthStack.Screen
+        name="Login"
+        component={Login}
+        options={() => ({
+          animation: 'slide_from_left',
+          headerShown: true,
+          headerStyle: {
+            backgroundColor: themeContext.colors.primary,
+          },
+        })}
+      />
+    </AuthStack.Navigator>
+  );
+};
