95eb8b78436823cfc0acdce9402ef29f5be4270d
Lucas Alves <as.lucasalves@gmail.com>
2023-03-29 00:48:28 -0300

    set default icon and name app on android and IOS

--- android/app/src/main/java/com/template/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
+++ android/app/src/main/java/com/template/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
@@ -41,7 +41,7 @@ public class MainApplicationTurboModuleManagerDelegate
     if (!sIsSoLibraryLoaded) {
       // If you change the name of your application .so file in the Android.mk file,
       // make sure you update the name here as well.
-      SoLoader.loadLibrary("apptemplate_appmodules");
+      SoLoader.loadLibrary("template_appmodules");
       sIsSoLibraryLoaded = true;
     }
   }
--- ios/TemplateTests/TemplateTests.m
+++ ios/TemplateTests/TemplateTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AppTemplateTests : XCTestCase
+@interface TemplateTests : XCTestCase
 
 @end
 
-@implementation AppTemplateTests
+@implementation TemplateTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
