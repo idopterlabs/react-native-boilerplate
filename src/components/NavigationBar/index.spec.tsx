cab8f3996a9376d84438b68a1fe3e7a579dc8818
Tiago Danin <TiagoDanin@outlook.com>
2023-04-05 10:46:42 -0300

    chore: create more tests to component navigation bar

--- src/components/NavigationBar/index.spec.tsx
+++ src/components/NavigationBar/index.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react-native';
+import { act, fireEvent, render } from '@testing-library/react-native';
 
 import { shadowTheme } from '@tests/actions/styledTheme';
 
@@ -8,6 +8,45 @@ import NavigationBar from '.';
 
 describe('Pages Card component', () => {
   it('should render correctly', async () => {
+    const { getByText, getByTestId } = render(
+      shadowTheme(
+        <NavigationBar
+          props={{
+            layout: {
+              width: 0,
+              height: 0,
+            },
+            back: { title: 'home' },
+            progress: {
+              // @ts-ignore
+              current: undefined,
+              next: undefined,
+              previous: undefined,
+            },
+            options: {
+              title: 'TITLE',
+              // @ts-ignore
+              actions: [{ iconName: 'logout', onPress: () => {} }],
+            },
+            // @ts-ignore
+            route: undefined,
+            // @ts-ignore
+            navigation: {
+              goBack: () => {},
+            },
+            // @ts-ignore
+            styleInterpolator: () => {},
+          }}
+        />,
+      ),
+    );
+
+    getByText('TITLE');
+    getByTestId('appBarActionView:navigationBar:back');
+    getByTestId('appBarActionView:navigationBar:logout');
+  });
+
+  it('should show default title when there was no title', async () => {
     const { getByText } = render(
       shadowTheme(
         <NavigationBar
@@ -23,15 +62,11 @@ describe('Pages Card component', () => {
               next: undefined,
               previous: undefined,
             },
-            options: {
-              title: 'TITLE',
-              // @ts-ignore
-              actions: [{ iconName: 'logout', onPress: () => {} }],
-            },
+            options: {},
             // @ts-ignore
             route: undefined,
             // @ts-ignore
-            navigation: undefined,
+            navigation: {},
             // @ts-ignore
             styleInterpolator: () => {},
           }}
@@ -39,6 +74,98 @@ describe('Pages Card component', () => {
       ),
     );
 
-    getByText('TITLE');
+    getByText('Dashboard');
+  });
+
+  it('should emit event click in back button', async () => {
+    const mockOnClick = jest.fn().mockImplementation(() => {});
+
+    const { getByTestId } = render(
+      shadowTheme(
+        <NavigationBar
+          props={{
+            layout: {
+              width: 0,
+              height: 0,
+            },
+            back: { title: 'home' },
+            progress: {
+              // @ts-ignore
+              current: undefined,
+              next: undefined,
+              previous: undefined,
+            },
+            options: {
+              title: 'TITLE',
+            },
+            // @ts-ignore
+            route: undefined,
+            // @ts-ignore
+            navigation: {
+              goBack: mockOnClick,
+            },
+            // @ts-ignore
+            styleInterpolator: () => {},
+          }}
+        />,
+      ),
+    );
+
+    expect(mockOnClick).toHaveBeenCalledTimes(0);
+
+    await act(async () => {
+      await fireEvent.press(
+        await getByTestId('appBarActionView:navigationBar:back'),
+      );
+    });
+
+    expect(mockOnClick).toHaveBeenCalledTimes(1);
+  });
+
+  it('should emit event click in action buttons', async () => {
+    const mockOnClick = jest.fn().mockImplementation(() => {});
+
+    const { getByTestId } = render(
+      shadowTheme(
+        <NavigationBar
+          props={{
+            layout: {
+              width: 0,
+              height: 0,
+            },
+            back: { title: 'home' },
+            progress: {
+              // @ts-ignore
+              current: undefined,
+              next: undefined,
+              previous: undefined,
+            },
+            options: {
+              title: 'TITLE',
+              // @ts-ignore
+              actions: [{ iconName: 'logout', onPress: mockOnClick }],
+            },
+            // @ts-ignore
+            route: undefined,
+            // @ts-ignore
+            navigation: {
+              goBack: () => {},
+            },
+            // @ts-ignore
+            styleInterpolator: () => {},
+          }}
+        />,
+      ),
+    );
+
+    expect(mockOnClick).toHaveBeenCalledTimes(0);
+
+    await act(async () => {
+      await fireEvent.press(
+        await getByTestId('appBarActionView:navigationBar:logout'),
+      );
+    });
+
+    expect(mockOnClick).toHaveBeenCalledTimes(1);
   });
 });
--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -15,22 +15,28 @@ export interface Props {
 
 export default ({ props }: Props) => {
   let isShowBackButton: boolean = !!props.back;
+
   // @ts-ignore
   if (props.options.isShowBackButton === false) {
     isShowBackButton = false;
   }
+
   return (
     <AppBarContainerView>
       {isShowBackButton && (
-        <AppBarBackActionView onPress={props.navigation.goBack} />
+        <AppBarBackActionView
+          onPress={props.navigation.goBack}
+          testID="appBarActionView:navigationBar:back"
+        />
       )}
-      <AppBarTitleView title={props.options.title || 'Wowlet'} />
+      <AppBarTitleView title={props.options.title || 'Dashboard'} />
 
       {/* @ts-ignore */}
       {props.options.actions?.map((action) => {
         return (
           <AppBarActionView
             key={`appBarActionView:navigationBar:${action.iconName}`}
+            testID={`appBarActionView:navigationBar:${action.iconName}`}
             icon={action.iconName}
             onPress={action.onPress}
           />
